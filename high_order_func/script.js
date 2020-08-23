e32fd07fe49472f536097b6db7238071fafe2475
fangg19 <alexjohnny6@gmail.com>
2020-08-23 20:36:02 +0300

    The project is done. Also added a sort option and a calculation function to get the total wealth.

--- high_order_func/script.js
+++ high_order_func/script.js
@@ -31,7 +31,14 @@ function doubleMoney() {
   data = data.map((user) => {
     return { ...user, money: user.money * 2 };
   });
-  console.log(data);
+  updateDOM();
+}
+
+//Show only users with a wealth over 1 million
+function showMillionaires() {
+  data = data.filter((user) => {
+    return user.money > 1000000;
+  });
   updateDOM();
 }
 
@@ -42,6 +49,19 @@ function sortByRichest() {
   });
 
   updateDOM();
+}
+
+//Calculate total wealth
+function calculateWealth() {
+  let wealth = data.reduce((initialValue, user) => {
+    return initialValue + user.money;
+  }, 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
 }
 
 //Add data function
@@ -59,17 +79,19 @@ function updateDOM(providedData = data) {
     element.classList.add('person');
     element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
       item.money
-    )} $`;
+    )}`;
     main.appendChild(element);
   });
 }
 
 //Format number as money
 function formatMoney(number) {
-  return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+  return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') + ' $';
 }
 
 //Event Listeners
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
